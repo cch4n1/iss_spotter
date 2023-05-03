fe6add25699657f3f4290e66acae3be3e61423f0
Charlie C <zbyabqrc@pm.me>
2023-05-03 09:59:11 +0000

    added fetchCoordsByIP function to get geo coordinates with ip.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log('Error:', error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  fetchCoordsByIP(ip, (error, gps) => {
+    if (error) {
+      return console.log("It didn't work!" , error);
+    }
+    console.log('It worked! Returned coordinates:' , gps);
+  });
 });
--- iss.js
+++ iss.js
@@ -1,34 +1,47 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-const request = require('request')
+const request = require('request');
 
-const fetchMyIP = function (callback) {
-  // use request to fetch IP address from JSON API
-  request(`https://api.ipify.org?format=json`, function (error, response, body) {
+// fetches ip address from JSON API
+const fetchMyIP = function(callback) {
+  request(`https://api.ipify.org?format=json`, function(error, response, body) {
     // check for domain connection error
     if (error) {
-      return callback(error, null)
+      return callback(error, null);
     }
     // if non-200 status, assume server error
-  if (response.statusCode !== 200) {
-    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    return callback(Error(msg), null);
-  }
-    const data = JSON.parse(body); 
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    const data = JSON.parse(body);
     // check if no data returns
     if (data.length === 0) {
-      return callback(`ip not found`, null)
-    };
-    // return ip
-    const ip = data.ip
-    return callback(null, ip)
+      return callback(`ip not found`, null);
+    }
+    // return ip address to callback
+    const ip = data.ip;
+    return callback(null, ip);
   });
-}
+};
 
-module.exports = { fetchMyIP };
+// fetches geo coordinates from JSON API based on ip address
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, function(error, response, body) {
+    if (error) {
+      return callback(error);
+    }
+    // saves data to gps object and returns to callback
+    const data = JSON.parse(body);
+    // error handling for invalid ip value
+    if (!data.success) {
+      const message = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
+      return callback(Error(message), null);
+    }
+    const gps = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+    return callback(null, gps);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
